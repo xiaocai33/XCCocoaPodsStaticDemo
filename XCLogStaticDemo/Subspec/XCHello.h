cae3a526bb8612aad0b61aa6f9573c3fdf56da12
caitengyuan <caitengyuan@bytedance.com>
2018-08-12 21:52:22 +0800

    添加类

---
+++ XCLogStaticDemo/Subspec/XCHello.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+@interface XCHello : NSObject
+
+- (void)xc_hello:(NSString *)msg;
+
+@end
---
+++ XCLogStaticDemo/Subspec/XCHello.m
@@ -0,0 +1,8 @@
+#import "XCHello.h"
+@implementation XCHello
+
+- (void)xc_hello:(NSString *)msg{
+    NSLog(@"xc_hello_%@", msg);
+}
+
+@end
